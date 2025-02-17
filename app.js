4df0ff657686952e54fd2fdcf32df2cde50fec04
Petterson Grande <grande.petterson@gmail.com>
2025-02-17 17:16:51 -0300

    Add more JS functionalities

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hi')
+const game = document.getElementById('gameBoard');
+// const allCards = document.querySelectorAll('.card');
+
+
+game.addEventListener('dblclick', (e)=>{
+    e.preventDefault();
+    if(e.target.tagName === 'DIV'){
+        console.log('This is a card');
+        e.target.classList.toggle('selectedCard');
+    };
+
+});
