76188429744c139ce8f1faaafce5666bf1e184f4
Petterson Grande <grande.petterson@gmail.com>
2025-02-25 12:28:15 -0300

    Find a way to .selectedCard get it's background-image from the allImgs object.

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const game = document.getElementById('gameBoard');
 // const allCards = document.querySelectorAll('.card');
 
+// Stored Images
+const allImgs = {
+    pabllo          : 'url(imgs/pablloVittar1.gif)',
+    blogueirinha    : 'url(imgs/blogueirinha1.gif)',
+    gretchen        : 'url(imgs/grethcen1.gif)',
+    inesBrasil      : 'url(imgs/inesBrasil1.gif)',
+    nazare          : 'url(imgs/nazare1.gif)',
+    tula            : 'url(imgs/tula1.gif)'
+};
+
+const {pabblo, blogueirinha, gretchen, inesBrasil, nazare, tula} = allImgs;
+
 
 game.addEventListener('dblclick', (e)=>{
     e.preventDefault();
     if(e.target.tagName === 'DIV'){
         console.log('This is a card');
-        e.target.classList.toggle('selectedCard');
+        e.target.classList.toggle('selectedCard'); 
+        
     };
 
 });
+
+// StoreImages
