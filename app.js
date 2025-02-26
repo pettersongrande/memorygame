1dc1673081dd055daef38828a9e18468588e0f79
Petterson Grande <grande.petterson@gmail.com>
2025-02-26 17:06:32 -0300

    Fix img sorting

--- app.js
+++ app.js
@@ -5,26 +5,23 @@ const allDivs = [];
 const imgs = [
     'url(imgs/pablloVittar1.gif)',
     'url(imgs/blogueirinha1.gif)',
-    'url(imgs/grethcen1.gif)',
+    'url(imgs/gretchen1.gif)',
     'url(imgs/inesBrasil1.gif)',
     'url(imgs/nazare1.gif)',
-    'url(imgs/tula1.gif)'
+    'url(imgs/tula1.gif)',
+
 ];
-const [pabllo, blogueirinha, gretchen, inesBrasil, nazare, tula] = imgs;
 
-
-renderGameBoard(game);
-
-// const allLinks = getLinks(imgs);
+renderGameBoard(game, imgs);
 
 // Event Listener ---------------------------------
 game.addEventListener('dblclick', (e)=>{
     e.preventDefault();
     if(e.target.tagName === 'DIV'){
-        // e.target.style.backgroundImage = linkintheattribute;
+        e.target.style.backgroundImage = e.target.dataset.src;
         e.target.classList.toggle('selectedCard'); 
         
-    };
+    }
 
 });
 
@@ -40,17 +37,34 @@ function divFactory(){
 
 //RENDER PAGE WITH NEW DIVS
 //Parameter 1 : area to append elements.
-//Parameter 2 : array with img links.
-function renderGameBoard(displayArea){
+function renderGameBoard(displayArea, arrOfImgs){
     displayArea.innerHTML = '';
-    
+    shuffle(imgs);
+
     for(let i = 0; i <=11; i++){
         const div = divFactory();
         div.innerHTML = 'CARD' + ' ' + i;       
         div.dataset.idx = allDivs.length;
-        div.dataset.src = imgs[i > 5 ? i - imgs.length : i];
+        div.dataset.src = imgs[i > 5 ? i - arrOfImgs.length : i];
         
         displayArea.append(div)
+        
     };
 
+    
+
 };
+
+// SHUFFLE
+function shuffle(arr){
+    let i = arr.length, j, temp;
+    while (--i > 0){
+        j = Math.floor(Math.random() * (i + 1));
+        temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+    
+}
+
+// const newImgs = shuffle(imgs);
