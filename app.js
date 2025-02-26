d7ddcd7bd1c53b0c5732efc80e5bc23693680e2d
Petterson Grande <grande.petterson@gmail.com>
2025-02-25 23:08:23 -0300

    Move to next functionality

--- app.js
+++ app.js
@@ -1,27 +1,56 @@
 const game = document.getElementById('gameBoard');
-// const allCards = document.querySelectorAll('.card');
+const allDivs = [];
 
 // Stored Images
-const allImgs = {
-    pabllo          : 'url(imgs/pablloVittar1.gif)',
-    blogueirinha    : 'url(imgs/blogueirinha1.gif)',
-    gretchen        : 'url(imgs/grethcen1.gif)',
-    inesBrasil      : 'url(imgs/inesBrasil1.gif)',
-    nazare          : 'url(imgs/nazare1.gif)',
-    tula            : 'url(imgs/tula1.gif)'
-};
+const imgs = [
+    'url(imgs/pablloVittar1.gif)',
+    'url(imgs/blogueirinha1.gif)',
+    'url(imgs/grethcen1.gif)',
+    'url(imgs/inesBrasil1.gif)',
+    'url(imgs/nazare1.gif)',
+    'url(imgs/tula1.gif)'
+];
+const [pabllo, blogueirinha, gretchen, inesBrasil, nazare, tula] = imgs;
 
-const {pabblo, blogueirinha, gretchen, inesBrasil, nazare, tula} = allImgs;
 
+renderGameBoard(game);
 
+// const allLinks = getLinks(imgs);
+
+// Event Listener ---------------------------------
 game.addEventListener('dblclick', (e)=>{
     e.preventDefault();
     if(e.target.tagName === 'DIV'){
-        console.log('This is a card');
+        // e.target.style.backgroundImage = linkintheattribute;
         e.target.classList.toggle('selectedCard'); 
         
     };
 
 });
 
-// StoreImages
+//DIV FACTORY: create a single DIV;
+function divFactory(){
+    const newDiv = document.createElement('DIV');
+        newDiv.classList.add('card');
+        allDivs.push(newDiv);
+        
+    return newDiv; 
+    
+};
+
+//RENDER PAGE WITH NEW DIVS
+//Parameter 1 : area to append elements.
+//Parameter 2 : array with img links.
+function renderGameBoard(displayArea){
+    displayArea.innerHTML = '';
+    
+    for(let i = 0; i <=11; i++){
+        const div = divFactory();
+        div.innerHTML = 'CARD' + ' ' + i;       
+        div.dataset.idx = allDivs.length;
+        div.dataset.src = imgs[i > 5 ? i - imgs.length : i];
+        
+        displayArea.append(div)
+    };
+
+};
